1480426464bb53e0b7d84a6ba16f68f27c29bdde
o3cloudng <o3cloudng@gmail.com>
2025-06-03 10:30:11 +0100

    CSRF trusted origin

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ from django.core.management.utils import get_random_secret_key
 # from decouple import config, Csv, AutoConfig
 import dj_database_url
 
-DEBUG=True
+DEBUG=False
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # config = AutoConfig(search_path='/home/tax-latest/.env')
