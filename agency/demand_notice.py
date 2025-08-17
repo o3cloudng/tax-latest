003e99350a30962b83d34da4213d11814bb84521
o3cloudng <o3cloudng@gmail.com>
2025-08-17 17:46:19 +0100

    Revised email  modified

--- agency/demand_notice.py
+++ agency/demand_notice.py
@@ -404,43 +404,43 @@ def agency_waiver(request, ref_id):
         else:
             messages.error(request, 'Waiver failed.')
 
-    if demand_notice.exists():
-        dn = demand_notice.get(Q(referenceid=ref_id))
-        form = WaiverForm(request.POST or None, request.FILES or None, instance=dn)
-        demand_notice = dn
-        penalty = dn.penalty
-        remittance = dn.remittance
-        waiver_applied = dn.waiver_applied
-        amount_paid = dn.amount_paid
-        amount_due = dn.amount_due
-        annual_fee = dn.annual_fee
-        total_liability = dn.total_due #- dn.waiver_applied
-        status = "REVISED"
+        if demand_notice.exists():
+            dn = demand_notice.get(Q(referenceid=ref_id))
+            form = WaiverForm(request.POST or None, request.FILES or None, instance=dn)
+            demand_notice = dn
+            penalty = dn.penalty
+            remittance = dn.remittance
+            waiver_applied = dn.waiver_applied
+            amount_paid = dn.amount_paid
+            amount_due = dn.amount_due
+            annual_fee = dn.annual_fee
+            total_liability = dn.total_due #- dn.waiver_applied
+            status = "REVISED"
 
 
-        # SEND EMAIL TO TAX PAYER (REVISED)
-        mail_subject = "REVISED DEMAND NOTICE BY AGENCY!"
-        to_email = company.email
-        agency = request.user
-        html_content = render_to_string("Emails/admin/revised_notice.html", {
-            "company":company,
-            "agency_email":agency,
-            "agency_phone":request.user.phone_number,
-            "referenceid":ref_id,
-            "waiver_applied":waiver_applied,
-            "total_liability":total_liability,
-            "login":settings.URL,
-            })
-        text_content = strip_tags(html_content)
-        send_email_function(html_content, text_content, to_email, mail_subject)
+            # SEND EMAIL TO TAX PAYER (REVISED)
+            mail_subject = "REVISED DEMAND NOTICE BY AGENCY!"
+            to_email = company.email
+            agency = request.user
+            html_content = render_to_string("Emails/admin/revised_notice.html", {
+                "company":company,
+                "agency_email":agency,
+                "agency_phone":request.user.phone_number,
+                "referenceid":ref_id,
+                "waiver_applied":waiver_applied,
+                "total_liability":total_liability,
+                "login":settings.URL,
+                })
+            text_content = strip_tags(html_content)
+            send_email_function(html_content, text_content, to_email, mail_subject)
 
-        # mail_subject = f"REVISED DEMAND NOTICE!: {ref_id}"
-        # email_template = "Emails/admin/revised_notice.html"
-        # agency_email_subject = f"NOTICE: REVISED DEMAND NOTICE - {company.company_name}"
-        # send_demand_notice_email(request, mail_subject, ref_id, dn.created_at, \
-        #                             total_due, email_template, agency_email_subject)
-    else:
-        form = WaiverForm(request.POST or None, request.FILES or None)
+            # mail_subject = f"REVISED DEMAND NOTICE!: {ref_id}"
+            # email_template = "Emails/admin/revised_notice.html"
+            # agency_email_subject = f"NOTICE: REVISED DEMAND NOTICE - {company.company_name}"
+            # send_demand_notice_email(request, mail_subject, ref_id, dn.created_at, \
+            #                             total_due, email_template, agency_email_subject)
+        else:
+            form = WaiverForm(request.POST or None, request.FILES or None)
 
     company = User.objects.get(id=demand_notice.company.id)
 
